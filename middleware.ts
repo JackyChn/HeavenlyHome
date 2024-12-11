21bb2ac7d50ac8a58ad9bc4196bbaafcd5e11bb8
JackyChn <ccc1020048076@gmail.com>
2024-12-11 22:36:58 +1030

    Add: middleware to redirect&protect the service page

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,15 @@
-import { auth } from "@/lib/auth";
-export const middleware = auth;
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+
+export async function middleware(req: any) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+  return NextResponse.next();
+}
 
 export const config = {
-  matcher: ["/homeService"],
+  matcher: ["/search/:path*"],
 };
